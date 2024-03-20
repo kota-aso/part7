6f1658b72e899c3e4a7588d9fe5cfc415d6fdb45
kota-aso <kotaaso1212@gmail.com>
2024-03-20 14:42:35 +0900

    Initial Commit

---
+++ part7/part7App.swift
@@ -0,0 +1,17 @@
+//
+//  part7App.swift
+//  part7
+//
+//  Created by Kota Aso on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct part7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
