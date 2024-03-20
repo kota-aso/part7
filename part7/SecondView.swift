62affc8388e19950993dd8e18c8d2cd1c50320ce
kota-aso <kotaaso1212@gmail.com>
2024-03-20 14:56:38 +0900

    firstCommit

---
+++ part7/SecondView.swift
@@ -0,0 +1,23 @@
+//
+//  SecondView.swift
+//  part7
+//
+//  Created by Kota Aso on 2024/03/20.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        ZStack {
+            Color(.orange)
+        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
